97075cbe66cfa2d3badcfd77795ef9e34f75dd91
Minhtandev <tannm20522002@gmail.com>
2022-10-08 11:00:53 +0700

    update navigation

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Flashcard } from './screens/Flashcard';
-import { LearnVoc } from './screens/LearnVoc';
-import { Minigame } from './screens/Minigame';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Flashcard } from "./src/screens/Flashcard";
+import { LearnVoc } from "./src/screens/LearnVoc";
+import { Minigame } from "./src/screens/Minigame";
+import { FlashcardDetail } from "./src/screens/FlashcardDetail";
+import { Vocabulary } from "./src/screens/Vocabulary";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-<NavigationContainer>
-      <Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'Welcome' }}
+          options={{ title: "Welcome" }}
         />
-        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
         <Stack.Screen name="Flashcard" component={Flashcard} />
         <Stack.Screen name="LearnVoc" component={LearnVoc} />
         <Stack.Screen name="Minigame" component={Minigame} />
+        <Stack.Screen name="FlashcardDetail" component={FlashcardDetail} />
+        <Stack.Screen name="Vocabulary" component={Vocabulary} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -29,25 +32,19 @@ export default function App() {
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
-    <Button
-      title="Go to Flashcard"
-      onPress={() =>
-        navigation.navigate('Flashcard', { name: 'Flashcard' })
-      }
-    />
-    <Button
-    title="Go to LearnVoc"
-    onPress={() =>
-      navigation.navigate('LearnVoc', { name: 'LearnVoc' })
-    }
-  />
-  <Button
-    title="Go to Minigame"
-    onPress={() =>
-      navigation.navigate('Minigame', { name: 'Minigame' })
-    }
-  />
-
+      <StatusBar hidden={true} />
+      <Button
+        title="Go to Flashcard"
+        onPress={() => navigation.navigate("Flashcard", { name: "Flashcard" })}
+      />
+      <Button
+        title="Go to LearnVoc"
+        onPress={() => navigation.navigate("LearnVoc", { name: "LearnVoc" })}
+      />
+      <Button
+        title="Go to Minigame"
+        onPress={() => navigation.navigate("Minigame", { name: "Minigame" })}
+      />
     </View>
   );
 };
@@ -55,8 +52,8 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/Flashcard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-export const Flashcard = ({ navigation, route }) => {
-  return (
-    <Text>This is {route.params.name}</Text>
-  )
-}
